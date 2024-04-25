e1d67e17bf694eb6a583f50fd75ac6053e871ccd
nzharkev <nzharkev@student.hive.fi>
2024-04-25 13:35:18 +0300

    memchr.c corrected

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:24:50 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/04/22 16:16:10 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/04/25 13:32:35 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,15 @@
 void	*ft_memchr(void const *ptr, int ch, size_t num)
 {
 	unsigned char	*t_ptr;
+	size_t			i;
 
+	i = 0;
 	t_ptr = (unsigned char *) ptr;
-	while (num > 0)
+	while (num > i)
 	{
-		if (*t_ptr == (char) ch)
+		if (*t_ptr == (unsigned char) ch)
 			return (t_ptr);
-		num--;
+		i++;
 		t_ptr++;
 	}
 	return (NULL);
