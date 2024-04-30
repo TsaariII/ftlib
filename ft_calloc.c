b802b54cdd71918e5e93a81035a93018a5163439
nzharkev <nzharkev@student.hive.fi>
2024-04-30 11:48:31 +0300

    Added test for size_max check

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,16 +6,19 @@
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:49:16 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/04/22 15:14:08 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/04/30 11:47:57 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <limits.h>
 
 void	*ft_calloc(size_t num_elem, size_t elem_size)
 {
 	void	*mem;
-
+	
+	if ((num_elem * elem_size) / num_elem > SIZE_MAX)
+		return (NULL);
 	mem = malloc(num_elem * elem_size);
 	if (!mem)
 		return (NULL);
