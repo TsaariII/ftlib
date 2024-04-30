88892ba12434598bb3e27ed401b0086123a98bf0
nzharkev <nzharkev@student.hive.fi>
2024-04-30 14:21:34 +0300

    Norm error fixed

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,13 @@
 /*   By: nzharkev <nzharkev@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 10:49:16 by nzharkev          #+#    #+#             */
-/*   Updated: 2024/04/30 14:03:26 by nzharkev         ###   ########.fr       */
+/*   Updated: 2024/04/30 14:21:20 by nzharkev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdint.h>
+
 void	*ft_calloc(size_t num_elem, size_t elem_size)
 {
 	void	*mem;
